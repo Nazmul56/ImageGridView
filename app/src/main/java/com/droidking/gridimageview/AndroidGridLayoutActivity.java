85a37ad9c9eaf27c963669ac7baa6607b79afc5e
nazmul56 <nazmul.cste07@gmail.com>
2016-05-27 22:47:19 +0600

    First

--- app/src/main/java/com/droidking/gridimageview/AndroidGridLayoutActivity.java
+++ app/src/main/java/com/droidking/gridimageview/AndroidGridLayoutActivity.java
@@ -28,7 +28,7 @@ public class AndroidGridLayoutActivity extends AppCompatActivity {
         // Instance of ImageAdapter Class
         gridView.setAdapter(new ImageAdapter(this));
         /**
-         * On Click event for Single Gridview Item
+         * On Click event for Single Gridview Item Listener
          * */
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
